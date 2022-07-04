28b013354ed3d4f0147027a244147bf74b52c6db
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-07-04 05:59:46 -0400

    add commit again

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
         <div className="App">
             <h1>Welcome WOrld</h1>
             <p>I hope this starts working</p>
-            <h3>Now I want to see what happens here</h3>
         </div>
     );
 }
